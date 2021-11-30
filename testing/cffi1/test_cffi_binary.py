b98aedc11cb616d47e7bfe1d9d28eccaa0b5394b
Matt Davis <mrd@redhat.com>
2021-11-29 17:45:40 -0800

    musl test fixes, all Linux wheels now static link private libffi build

--- testing/cffi1/test_cffi_binary.py
+++ testing/cffi1/test_cffi_binary.py
@@ -1,10 +1,11 @@
 import py, sys, os
 import _cffi_backend
+from testing.support import is_musl
 
 def test_no_unknown_exported_symbols():
     if not hasattr(_cffi_backend, '__file__'):
         py.test.skip("_cffi_backend module is built-in")
-    if not sys.platform.startswith('linux'):
+    if not sys.platform.startswith('linux') or is_musl:
         py.test.skip("linux-only")
     g = os.popen("objdump -T '%s'" % _cffi_backend.__file__, 'r')
     for line in g:
@@ -16,6 +17,9 @@ def test_no_unknown_exported_symbols():
             continue
         name = line.split()[-1]
         if name.startswith('_') or name.startswith('.'):
+            continue
+        # a statically-linked libffi will always appear here without header hackage, ignore it if it's internal
+        if name.startswith('ffi_') and 'Base' in line:
             continue
         if name not in ('init_cffi_backend', 'PyInit__cffi_backend'):
             raise Exception("Unexpected exported name %r" % (name,))
